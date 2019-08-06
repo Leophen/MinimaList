937cc63ed5fe621bf1abb71e1024abc86fd8e1cb
Leophen <Leophen@foxmail.com>
2019-08-06 16:31:11 +0800

    update-1.1

---
+++ MinimaList-1.1/src/store.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import moment from 'moment'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    lists: []
+  },
+  mutations: {
+    addItem (state, value) {
+      value.time = moment(value.time).format('YYYY-MM-DD HH:mm')
+      state.lists.push(value)
+    },
+    removeItem (state, value) {
+      state.lists.splice(value, 1)
+    }
+  },
+  actions: {
+
+  }
+})
