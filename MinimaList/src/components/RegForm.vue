e80a52a574c582697e3ccaceaff6c4b16ac89554
陈培珊 <honey@bogon.dhcp>
2019-10-05 13:24:32 +0800

    newchange

---
+++ MinimaList/src/components/RegForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <form class="form-wrapper">
+    <div class="info">快速注册</div>
+    <div class="form-input">
+      <label>用户名</label>
+      <input type="text" v-model="name">
+    </div>
+    <div class="form-input">
+      <label>密码</label>
+      <input type="password" v-model="password">
+    </div>
+    <div class="form-input">
+      <label>确认密码</label>
+      <input type="password" v-model="passwordRepeart">
+    </div>
+    <div class="btns">
+      <button type="button" @click="reg()">注册</button>
+      <button type="button" @click="()=>{this.$router.back()}">取消</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'RegForm',
+  data () {
+    return {
+      name: '',
+      password: '',
+      passwordRepeart: ''
+    }
+  },
+  methods: {
+    reg () {
+      if (this.password === this.passwordRepeart && this.name !== '') {
+        localStorage.setItem('name', this.name)
+        localStorage.setItem('password', this.password)
+        this.$router.push({ name: 'login' })
+      } else {
+        alert('请检查密码是否输入一致')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.info{
+  padding: 30px;
+  font-size: 18px;
+  font-weight: bold;
+  color: #757575;
+}
+.btns{
+  padding-top: 30px;
+}
+</style>
---
+++ MinimaList/src/views/User.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="user">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <div class="name">{{name}}</div>
+      <p>欢迎进入个人中心</p>
+    </div>
+    <div class="ctrls">
+      <router-link :to="{name: 'Add'}" class="Add">添加事项</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data () {
+    return {
+      name: ''
+    }
+  },
+  mounted () {
+    this.name = localStorage.getItem('name') ? localStorage.getItem('name') : 'VueProject'
+  }
+}
+</script>
+
+<style scoped lang="scss">
+$main-color: #42b983;
+.user{
+  text-align: center;
+  font-size: 18px;
+  position: relative;
+  top: 70px;
+  img{
+    width: 150px;
+    height: 150px;
+  }
+  .name{
+    margin-top: 20px;
+    padding: 10px;
+    color: #757575;
+  }
+  p{
+    color: #757575;
+  }
+}
+.ctrls{
+  width: 50%;
+  position: relative;
+  left: 25%;
+  top: 120px;
+  text-align: center;
+}
+.Add{
+  display: block;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 10px;
+  background: $main-color;
+  margin: 0 auto;
+  color: #fff;
+}
+</style>
